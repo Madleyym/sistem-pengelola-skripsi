741516a81c0773b11fcb159c3efb51a0ec24f1dc
Madleyym <tryandaasu@gmail.com>
2025-02-04 21:40:26 +0700

    Add routes for Skripsi management with authentication

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\SkripsiController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -26,6 +27,13 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+Route::middleware('auth')->group(function () {
+    Route::get('/skripsi', [SkripsiController::class, 'index'])->name('skripsi.index');
+    Route::get('/skripsi/create', [SkripsiController::class, 'create'])->name('skripsi.create');
+    Route::post('/skripsi', [SkripsiController::class, 'store'])->name('skripsi.store');
+    Route::get('/skripsi/{skripsi}', [SkripsiController::class, 'show'])->name('skripsi.show');
 });
 
 require __DIR__.'/auth.php';
