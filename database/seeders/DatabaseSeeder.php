4fdc364a4e0d212d2dcb9c202c5891610aae009a
Agisna Revaldo <agisrifaldo75@gmail.com>
2025-02-05 00:23:52 +0700

    refactor DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,7 +4,6 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
-use App\Models\Skripsi;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -17,9 +16,14 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call([
-            UserSeeder::class,
-            SkripsiSeeder::class,
-            // Tambahkan seeder lain di sini
+            UserSeeder::class,    // 1. Buat user lebih dulu
+            JurusanSeeder::class,  // 2. Jurusan dibuat lebih dulu
+            DosenSeeder::class,    // 3. Dosen membutuhkan user dan jurusan
+            MahasiswaSeeder::class, // 4. Mahasiswa membutuhkan user dan jurusan
+            SkripsiSeeder::class,  // 5. Skripsi membutuhkan mahasiswa
+            SeminarSeeder::class,  // 6. Seminar membutuhkan skripsi
+            SidangSeeder::class,   // 7. Sidang membutuhkan skripsi dan dosen
+            BimbinganSeeder::class // 8. Bimbingan membutuhkan dosen dan mahasiswa
         ]);
     }
 }
