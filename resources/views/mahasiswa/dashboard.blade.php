5b4dde4c5c601439b2ca1b6d0a08c0435dcc6531
Madleyym <tryandaasu@gmail.com>
2025-02-10 05:22:28 +0700

    Implement logout functionality with CSRF protection in dashboard

--- resources/views/mahasiswa/dashboard.blade.php
+++ resources/views/mahasiswa/dashboard.blade.php
@@ -153,7 +153,12 @@
                     <li><a class="dropdown-item" href="#">Profil</a></li>
                     <li><a class="dropdown-item" href="#">Pengaturan</a></li>
                     <li><hr class="dropdown-divider"></li>
-                    <li><a class="dropdown-item" href="#">Logout</a></li>
+                    <li>
+                            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                @csrf
+                            </form>
+                            <a class="dropdown-item" href="#" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">Logout</a>
+                        </li>
                 </ul>
             </div>
         </div>
